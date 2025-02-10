ac47f27ecf401f13b24bebe11dd1e1ca5355232a
arthurMalveste <arthurmielemalveste@gmail.com>
2025-02-10 11:36:39 -0300

    criacao de uma tabela para salvar as respostas

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require("body-parser")
 const connection = require("./database/database")
 const Pergunta = require("./database/pergunta")
+const Resposta = require("./database/Resposta")
 
 var menssagem = false
 
